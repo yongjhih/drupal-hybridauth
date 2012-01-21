8d8d7d013f060621a86c747b40c7ab12cd17e3ae
Robert Wohleb <rwohleb@gmail.com>
2012-01-20 16:44:03 -0800

    Finished provider list popup. Cleaned up provider popup.

--- hybridauth-popup-list.tpl.php
+++ hybridauth-popup-list.tpl.php
@@ -1,1 +1,6 @@
-List of providers...
+<h3>Sign in using your account with:</h3>
+<ul id="<?php print $widget_id; ?>" class="<?php print $classes; ?>">
+<?php foreach ($providers as $provider_id => $icon) { ?>
+  <li><?php print $icon; ?></li>
+<?php } ?>
+</ul>
--- hybridauth-popup-provider.tpl.php
+++ hybridauth-popup-provider.tpl.php
@@ -1,11 +1,11 @@
 
-<table>
-  <tr>
-    <td><?php print $provider_icon; ?></td>
-    <td><?php print $provider_name; ?></td>
-  </tr>
-  <tr>
-    <td><?php print $cancel_button; ?></td>
-    <td><?php print $provider_button; ?></td>
-  </tr>
-</table>
+<div id="<?php print $popup_id; ?>" class="<?php print $classes; ?>">
+  <div class="info">
+    <div class="icon"><?php print $provider_icon; ?></div>
+    <div class="name"><?php print $provider_name; ?></div>
+  </div>
+  <div class="buttons">
+    <div class="button login"><?php print $provider_button; ?></div>
+    <div class="button cancel"><?php print $cancel_button; ?></div>
+  </div>
+</div>
