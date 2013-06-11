25e7a7562a9b668206647a1ff2fc9956911475c8
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-06-11 15:56:12 +0400

    Added basic API functions and hooks description.

---
+++ hybridauth.api.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Description of HybridAuth API functions and hooks.
+ */
+
+/**
+ * Use this function to get HybridAuth instance or error code.
+ */
+function hybridauth_get_instance() {
+
+}
+
+/**
+ * Alter the generated username for the user being created by HybridAuth.
+ */
+function hook_hybridauth_username_alter(&$name, $data) {
+
+}
+
+/**
+ * Alter the values returned by HybridAuth library.
+ */
+function hook_hybridauth_profile_alter(&$profile) {
+
+}
+
+/**
+ * Alter the user info for the user being created by HybridAuth.
+ */
+function hook_hybridauth_userinfo_alter(&$userinfo, $data) {
+
+}
+
+/**
+ * Should we show the registration form or not?
+ * You should also implement hook_form_hybridauth_additional_info_form_alter()
+ * to add your form elements, validate and submit callbacks to this form.
+ * See hybridauth_bonus module for example usage.
+ */
+function hook_hybridauth_registration_form($data) {
+
+}
