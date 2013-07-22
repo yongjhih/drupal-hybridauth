6c5b06c89a461b4e12c38eaa0f9cce19509b4c4f
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-07-22 13:58:25 +0400

    implemented [#2042307] - added 4 more hooks for corresponding Rules events.

--- hybridauth.api.php
+++ hybridauth.api.php
@@ -13,6 +13,8 @@ function hybridauth_get_instance() {
 
 /**
  * Alter the generated username for the user being created by HybridAuth.
+ * @param $name
+ * @param $data - HybridAuth identity data
  */
 function hook_hybridauth_username_alter(&$name, $data) {
 
@@ -20,6 +22,7 @@ function hook_hybridauth_username_alter(&$name, $data) {
 
 /**
  * Alter the values returned by HybridAuth library.
+ * @param $profile
  */
 function hook_hybridauth_profile_alter(&$profile) {
 
@@ -27,6 +30,8 @@ function hook_hybridauth_profile_alter(&$profile) {
 
 /**
  * Alter the user info for the user being created by HybridAuth.
+ * @param $userinfo
+ * @param $data - HybridAuth identity data
  */
 function hook_hybridauth_userinfo_alter(&$userinfo, $data) {
 
@@ -47,7 +52,44 @@ function hook_hybridauth_registration_form($data) {
  * based on the data received from authentication provider.
  * Return translated message to show to the user if the registration should be
  * blocked. Return just TRUE if you don't want to show any message.
+ * @param $data - HybridAuth identity data
  */
 function hook_hybridauth_registration_block($data) {
   
+}
+
+/**
+ * Invoked when added new HybridAuth identity to user account.
+ * @param $account - user account object
+ * @param $data - HybridAuth identity data
+ */
+function hook_hybridauth_identity_added($account, $data) {
+
+}
+
+/**
+ * Invoked when deleted HybridAuth identity from user account.
+ * @param $account - user account object
+ * @param $data - HybridAuth identity data
+ */
+function hook_hybridauth_identity_deleted($account, $data) {
+
+}
+
+/**
+ * Invoked when created a new user account through HybridAuth.
+ * @param $account - user account object
+ * @param $data - HybridAuth identity data
+ */
+function hook_hybridauth_user_insert($account, $data) {
+
+}
+
+/**
+ * Invoked when logged in a user account through HybridAuth.
+ * @param $account - user account object
+ * @param $data - HybridAuth identity data
+ */
+function hook_hybridauth_user_login($account, $data) {
+
 }
