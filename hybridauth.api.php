898924a17fae4d03f769a834b96f8e5276f3bddf
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-07-26 12:34:08 +0400

    Added new hooks to hook_hook_info(). Changes to API functions and hooks documentation.

--- hybridauth.api.php
+++ hybridauth.api.php
@@ -13,8 +13,9 @@ function hybridauth_get_instance() {
 
 /**
  * Alter the generated username for the user being created by HybridAuth.
- * @param $name
- * @param $data - HybridAuth identity data
+ * @param string $name
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_username_alter(&$name, $data) {
 
@@ -22,7 +23,7 @@ function hook_hybridauth_username_alter(&$name, $data) {
 
 /**
  * Alter the values returned by HybridAuth library.
- * @param $profile
+ * @param array $profile
  */
 function hook_hybridauth_profile_alter(&$profile) {
 
@@ -30,8 +31,10 @@ function hook_hybridauth_profile_alter(&$profile) {
 
 /**
  * Alter the user info for the user being created by HybridAuth.
- * @param $userinfo
- * @param $data - HybridAuth identity data
+ * @param array $userinfo
+ *   Information to create a new user.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_userinfo_alter(&$userinfo, $data) {
 
@@ -42,6 +45,8 @@ function hook_hybridauth_userinfo_alter(&$userinfo, $data) {
  * You should also implement hook_form_hybridauth_additional_info_form_alter()
  * to add your form elements, validate and submit callbacks to this form.
  * See hybridauth_bonus module for example usage.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_registration_form($data) {
 
@@ -52,7 +57,8 @@ function hook_hybridauth_registration_form($data) {
  * based on the data received from authentication provider.
  * Return translated message to show to the user if the registration should be
  * blocked. Return just TRUE if you don't want to show any message.
- * @param $data - HybridAuth identity data
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_registration_block($data) {
   
@@ -60,8 +66,10 @@ function hook_hybridauth_registration_block($data) {
 
 /**
  * Invoked when added new HybridAuth identity to user account.
- * @param $account - user account object
- * @param $data - HybridAuth identity data
+ * @param object $account
+ *   User account object.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_identity_added($account, $data) {
 
@@ -69,26 +77,32 @@ function hook_hybridauth_identity_added($account, $data) {
 
 /**
  * Invoked when deleted HybridAuth identity from user account.
- * @param $account - user account object
- * @param $data - HybridAuth identity data
+ * @param object $account
+ *   User account object.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_identity_deleted($account, $data) {
 
 }
 
 /**
- * Invoked when created a new user account through HybridAuth.
- * @param $account - user account object
- * @param $data - HybridAuth identity data
+ * Invoked when a new user account is created through HybridAuth.
+ * @param object $account
+ *   User account object.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_user_insert($account, $data) {
 
 }
 
 /**
- * Invoked when logged in a user account through HybridAuth.
- * @param $account - user account object
- * @param $data - HybridAuth identity data
+ * Invoked when a user has logged in through HybridAuth.
+ * @param object $account
+ *   User account object.
+ * @param array $data
+ *   HybridAuth identity data.
  */
 function hook_hybridauth_user_login($account, $data) {
 
