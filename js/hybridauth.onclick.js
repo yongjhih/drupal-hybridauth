1421330830c51973afcdc9579a7c895b04f63ae1
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-10-17 17:53:18 +0400

    Implemented [#1417880] - mark last used provider.

--- js/hybridauth.onclick.js
+++ js/hybridauth.onclick.js
@@ -2,10 +2,10 @@
 
   Drupal.behaviors.hybridauth_onclick = {};
   Drupal.behaviors.hybridauth_onclick.attach = function(context, settings) {
-    $('.hybridauth-onclick-current', context).bind('click', function(index, Element) {
+    $('.hybridauth-onclick-current', context).bind('click', function() {
       $(this).parents('.hybridauth-widget').after('<div>' + Drupal.t('Contacting') + ' ' + this.title + '...</div>');
     });
-    $('.hybridauth-onclick-popup', context).bind('click', function(index, Element) {
+    $('.hybridauth-onclick-popup', context).bind('click', function() {
       var width = $(this).attr('data-hybridauth-width'), height = $(this).attr('data-hybridauth-height');
       var popup_window = window.open(
         this.href,
@@ -14,6 +14,15 @@
       );
       popup_window.focus();
       return false;
+    });
+
+    // Last used provider feature.
+    var last_provider = $.cookie('hybridauth_last_provider');
+    if (last_provider != null) {
+      $('[data-hybridauth-provider="' + last_provider + '"]').addClass('hybridauth-last-provider');
+    }
+    $('.hybridauth-widget-provider', context).bind('click', function() {
+      $.cookie('hybridauth_last_provider', $(this).attr('data-hybridauth-provider'), {expires: 30, path: '/'});
     });
   };
 
