b5847378a0b562a6f11dcfbf2fe0ae1cfea24bbc
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-10-17 13:00:35 +0400

    Moved all the JS code to Drupal.behaviors - see [#1974274].

--- js/hybridauth.onclick.js
+++ js/hybridauth.onclick.js
@@ -2,12 +2,19 @@
 
   Drupal.behaviors.hybridauth_onclick = {};
   Drupal.behaviors.hybridauth_onclick.attach = function(context, settings) {
-    $('.hybridauth-onclick-current', context).click(function(index, Element) {
+    $('.hybridauth-onclick-current', context).bind('click', function(index, Element) {
       $(this).parents('.hybridauth-widget').after('<div>' + Drupal.t('Contacting') + ' ' + this.title + '...</div>');
     });
-    /*$('.hybridauth-onclick-popup', context).click(function(index, Element) {
-      
-    });*/
+    $('.hybridauth-onclick-popup', context).bind('click', function(index, Element) {
+      var width = $(this).attr('data-hybridauth-width'), height = $(this).attr('data-hybridauth-height');
+      var popup_window = window.open(
+        this.href,
+        'hybridauth',
+        'location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=yes,toolbar=no,channelmode=yes,fullscreen=yes,width=' + width + ',height=' + height
+      );
+      popup_window.focus();
+      return false;
+    });
   };
 
 })(jQuery);
